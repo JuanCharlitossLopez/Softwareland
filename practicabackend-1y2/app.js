3c245de37cf1022c4d95330248e50c4367f1d11b
Juan Carlos López León <juancalopez825@gmail.com>
2024-08-01 10:15:32 -0600

    Practica #2 - Endpoints

--- practicabackend-1y2/app.js
+++ practicabackend-1y2/app.js
@@ -2,17 +2,49 @@ const express = require('express')
 const app = express()
 const port = 3002
 
-app.get('/saludo', (req, res) => {
-  const {name, lastName} =req.query;
+// app.get('/saludo', (req, res) => {
+//   const {name, lastName} =req.query;
 
-  if (name && lastName) {
-    res.send(`Hola ${name} ${lastName}`);
-  } else {
-    res.status(400).send('Por favor, proporciona los parámetros "name" y "lastName".');
-  }
-  // res.send('Hello World!')
-})
+//   if (name && lastName) {
+//     res.send(`Hola ${name} ${lastName}`);
+//   } else {
+//     res.status(400).send('Por favor, proporciona los parámetros "name" y "lastName".');
+//   }
+//   // res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+//para parsear el cuerpo de las solicitudes
+app.use(express.json());
+
+// Ruta GET para 'users'
+app.get('/users', (req, res) => {
+    res.send('Respuesta a una solicitud GET en /users');
+});
+
+// Ruta POST para 'users'
+app.post('/users', (req, res) => {
+    res.send('Respuesta a una solicitud POST en /users');
+});
+
+// Ruta PUT para 'users'
+app.put('/users', (req, res) => {
+    res.send('Respuesta a una solicitud PUT en /users');
+});
+
+// Ruta DELETE para 'users'
+app.delete('/users', (req, res) => {
+    res.send('Respuesta a una solicitud DELETE en /users');
+});
+
+//para manejar solicitudes a rutas no definidas (404)
+app.use((req, res, next) => {
+    res.status(404).send('Error 404: Endpoint no encontrado');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Servidor escuchando en http://localhost:${port}`);
+});
