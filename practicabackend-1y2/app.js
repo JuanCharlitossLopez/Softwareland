b619672c0346c566a1ab91e6b86931356e8fb411
Juan Carlos López León <juancalopez825@gmail.com>
2024-08-07 09:53:54 -0600

    Practica #3 - Transacciones endpoints

--- practicabackend-1y2/app.js
+++ practicabackend-1y2/app.js
@@ -1,6 +1,93 @@
-const express = require('express')
-const app = express()
-const port = 3002
+const express = require("express");
+const app = express();
+const port = 3002;
+// FILE SYSTEM
+const fs = require("fs");
+// Ubicacion del archivo
+let jsonData = fs.readFileSync("./data/users.json");
+// parsear buffer de datos
+let users = JSON.parse(jsonData);
+console.log(users);
+
+app.use(express.json());
+
+//GET users
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+//GET users/id
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const user = users.find((user) => user.id === parseInt(id));
+  if (!user) {
+    res.status(404).json({ message: "User not found" });
+  } else {
+    res.json(user);
+  }
+});
+
+// POST users
+app.post("/users", (req, res) => {
+  const { id, password, first_name, last_name, username, email, gender } =
+    req.body;
+  const user = {
+    id,
+    password,
+    first_name,
+    last_name,
+    username,
+    email,
+    gender,
+  };
+  users.push(user);
+  fs.writeFileSync("./data/users.json", JSON.stringify(users));
+  res.status(201).json(user);
+});
+
+// DELETE users
+app.delete("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const index = users.findIndex((user) => user.id === parseInt(id));
+  if (index === -1) {
+    res.status(404).json({ message: "User not found" });
+  } else {
+    users.splice(index, 1);
+    fs.writeFileSync("./data/users.json", JSON.stringify(users));
+    res.status(204).json({ message: "User deleted" });
+  }
+});
+
+// PUT users
+app.put("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const index = users.findIndex((user) => user.id === parseInt(id));
+  if (index === -1) {
+    res.status(404).json({ message: "User not found" });
+  } else {
+    const { password, first_name, last_name, username, email, gender } =
+      req.body;
+    users[index] = {
+      id,
+      password,
+      first_name,
+      last_name,
+      username,
+      email,
+      gender,
+    };
+    fs.writeFileSync("./data/users.json", JSON.stringify(users));
+    res.status(200).json(users[index]);
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("root");
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
 
 // app.get('/saludo', (req, res) => {
 //   const {name, lastName} =req.query;
@@ -18,33 +105,29 @@ const port = 3002
 // })
 
 //para parsear el cuerpo de las solicitudes
-app.use(express.json());
+// app.use(express.json());
 
-// Ruta GET para 'users'
-app.get('/users', (req, res) => {
-    res.send('Respuesta a una solicitud GET en /users');
-});
+// // Ruta GET para 'users'
+// app.get('/users', (req, res) => {
+//     res.send('Respuesta a una solicitud GET en /users');
+// });
 
-// Ruta POST para 'users'
-app.post('/users', (req, res) => {
-    res.send('Respuesta a una solicitud POST en /users');
-});
+// // Ruta POST para 'users'
+// app.post('/users', (req, res) => {
+//     res.send('Respuesta a una solicitud POST en /users');
+// });
 
-// Ruta PUT para 'users'
-app.put('/users', (req, res) => {
-    res.send('Respuesta a una solicitud PUT en /users');
-});
+// // Ruta PUT para 'users'
+// app.put('/users', (req, res) => {
+//     res.send('Respuesta a una solicitud PUT en /users');
+// });
 
-// Ruta DELETE para 'users'
-app.delete('/users', (req, res) => {
-    res.send('Respuesta a una solicitud DELETE en /users');
-});
+// // Ruta DELETE para 'users'
+// app.delete('/users', (req, res) => {
+//     res.send('Respuesta a una solicitud DELETE en /users');
+// });
 
-//para manejar solicitudes a rutas no definidas (404)
-app.use((req, res, next) => {
-    res.status(404).send('Error 404: Endpoint no encontrado');
-});
-
-app.listen(port, () => {
-    console.log(`Servidor escuchando en http://localhost:${port}`);
-});
+// //para manejar solicitudes a rutas no definidas (404)
+// app.use((req, res, next) => {
+//     res.status(404).send('Error 404: Endpoint no encontrado');
+// });
